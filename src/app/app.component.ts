ab4f9df0e1a0fbe029c5fd254253b870f723964f
sunilkumarchandi87 <sunilkumar.chandi87@gmail.com>
2021-11-23 13:14:59 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component, VERSION } from '@angular/core';
+//import mydata from './testjson.json';
 
+interface MyInterface {
+  id: Number;
+  firstname: String;
+  lastname: String;
+  email: String;
+  gender: String;
+}
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  //mydatalst: MyInterface[] = mydata;
 }
